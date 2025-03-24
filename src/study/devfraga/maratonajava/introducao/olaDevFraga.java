8ec228c28c47fbf1538569650bdc23934487e9b8
Lucas Fraga <lucasfraga.dev@gmail.com>
2025-03-24 19:32:03 -0300

    Aula 08: organizando pacotes

---
+++ src/study/devfraga/maratonajava/introducao/olaDevFraga.java
@@ -0,0 +1,7 @@
+package study.devfraga.maratonajava.introducao; // A primeira linha de código deve ser a do pacote
+
+public class olaDevFraga {
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HAAA");
+    }
+}
